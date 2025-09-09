1019c262705e9b527d21460d2b09c483092132bd
divyesh2006d <divyesh2006d@gmail.com>
2025-09-09 10:19:04 +0530

    first commit(working)

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from turtle import Turtle, Screen
+
+# Setup screen
+
+s = Screen()
+s.setup(width=725, height=491)
+s.bgpic("blank_states_img.gif")
+
+# Load CSV into DataFrame
+file = pd.read_csv("50_states.csv")
+print(file)
+# Get user input
+
+guss = []
+while len(guss) < 50:
+
+    answer_state = s.textinput(title="US State Game", prompt="Enter a state:").title()
+
+    if answer_state is None:
+        break
+
+    if answer_state == "Exit":
+        break
+
+    # If the state exists in CSV
+    if answer_state in file.state.values and answer_state not in guss:
+        guss.append(answer_state)
+        # Get row for that state
+        state_data = file[file.state == answer_state]
+
+        # Create a turtle to write the name
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        t.goto(int(state_data.x), int(state_data.y))   # move to state coords
+        t.write(answer_state, align="center", font=("Arial", 10, "normal"))
+
+s.exitonclick()
